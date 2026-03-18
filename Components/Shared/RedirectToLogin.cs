74fab9e5bc28362efb36707f21914a92c1612f7a
Pedro Rodríguez <pedro.w0rking@hotmail.com>
2026-03-18 17:30:10 +0100

    Update: RedirectToLogin

---
+++ Components/Shared/RedirectToLogin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MundialDosmilVeintiSeis.Components.Shared
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager Nav { get; set; } = default!;
+        protected override void OnInitialized()
+        {
+            Nav.NavigateTo("/login",forceLoad: false);
+        }
+    }
+}
